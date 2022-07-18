62c0eacd550f8cf47c61cb64c783567b26293afd
G3 <aljawharayr@gmail.com>
2022-07-18 14:28:34 +0300

    //

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from 'src/environments/environment';
+import { environment } from 'environments/environment';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HeraderComponent } from './herader/herader.component';
 import { HttpClientModule } from '@angular/common/http';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebase : {
-    apiKey: "AIzaSyCB_lRp6hdSnnvglgzgXaW8Aal8E9xc0XM",
-    authDomain: "fir-angular-5c94b.firebaseapp.com",
-    projectId: "fir-angular-5c94b",
-    storageBucket: "fir-angular-5c94b.appspot.com",
-    messagingSenderId: "958031621427",
-    appId: "1:958031621427:web:5664cad914fe0f5dd52246"
+  firebase: {
+    projectId: 'fir-angular-5c94b',
+    appId: '1:958031621427:web:5664cad914fe0f5dd52246',
+    storageBucket: 'fir-angular-5c94b.appspot.com',
+    apiKey: 'AIzaSyCB_lRp6hdSnnvglgzgXaW8Aal8E9xc0XM',
+    authDomain: 'fir-angular-5c94b.firebaseapp.com',
+    messagingSenderId: '958031621427',
   }
 
   
